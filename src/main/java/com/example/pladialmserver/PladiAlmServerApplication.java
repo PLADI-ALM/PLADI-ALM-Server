a5e776b336373290c029616a39ce21998593d5d8
iseunghag <qw0802@naver.com>
2023-09-14 02:17:17 +0900

    initalize PLADI-ALM-Service

---
+++ src/main/java/com/example/pladialmserver/PladiAlmServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pladialmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PladiAlmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PladiAlmServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pladialmserver/PladiAlmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pladialmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PladiAlmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
