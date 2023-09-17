8f61b9db2541e7b771651ef4fdf993b07b724539
sojungpp <sophie2458@naver.com>
2023-09-17 16:44:42 +0900

    [PLD-12] feat: User Entity 생성

---
+++ src/main/java/com/example/pladialmserver/global/user/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.pladialmserver.global.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(length = 30)
+    private String name;
+
+}
