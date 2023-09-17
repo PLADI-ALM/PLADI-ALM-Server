fab3a532031d25e5b091510690bf9cb5f2218cf7
sojungpp <sophie2458@naver.com>
2023-09-17 17:37:17 +0900

    [PLD-12] feat: User 관련 MVC 패턴 추가

---
+++ src/main/java/com/example/pladialmserver/global/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.pladialmserver.global.user.controller;
+
+import com.example.pladialmserver.global.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+}
