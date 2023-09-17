bc7489ee8ec4e7f192790613981953aed9a73a1c
sojungpp <sophie2458@naver.com>
2023-09-17 17:32:24 +0900

    [PLD-12] feat: User Entity에 OfficeBooking 매핑

--- src/main/java/com/example/pladialmserver/global/user/entity/User.java
+++ src/main/java/com/example/pladialmserver/global/user/entity/User.java
@@ -1,10 +1,12 @@
 package com.example.pladialmserver.global.user.entity;
 
+import com.example.pladialmserver.office.entity.OfficeBooking;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,5 +19,8 @@ public class User {
 
     @Column(length = 30)
     private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<OfficeBooking> officeBookingList;
 
 }
