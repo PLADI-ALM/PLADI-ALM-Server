97729f777515d73747f58dc1f632e9257390d500
chaerlo127 <monynony0203@gmail.com>
2023-09-25 19:11:24 +0900

    [PDS-14] feat: 회의실 예약 스웨거 설정 추가

--- src/main/java/com/example/pladialmserver/office/controller/OfficeController.java
+++ src/main/java/com/example/pladialmserver/office/controller/OfficeController.java
@@ -9,6 +9,7 @@ import com.example.pladialmserver.office.dto.response.OfficeRes;
 import com.example.pladialmserver.office.service.OfficeService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -63,7 +64,7 @@ public class OfficeController {
     @Operation(summary = "회의실 개별 조회", description = "회의실 개별 조회를 진행한다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "(S0001)회의실 목록 조회 성공", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
-            @ApiResponse(responseCode = "400", description = "(O0001)존재하지 않는 회의실입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "404", description = "(O0001)존재하지 않는 회의실입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
     })
     @GetMapping("/{officeId}")
     public ResponseCustom<OfficeRes> getOffice(@PathVariable(name="officeId") Long officeId){
@@ -85,9 +86,17 @@ public class OfficeController {
     /**
      * 회의실 예약
      */
+    @Operation(summary = "회의실 예약", description = "회의실을 예약한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "(S0001)회의실 목록 조회 성공", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "400", description = "(B0001)날짜와 시간을 모두 입력해주세요. (B0002) 요청사항은 30자 이하로 작성해주세요. (B0003)시작시간보다 끝나는 시간이 더 앞에 있습니다. (B0004)미래의 날짜를 선택해주세요.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "404", description = "(O0001)존재하지 않는 회의실입니다. (U0001)사용자를 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "409", description = "(B0005)이미 예약되어 있는 시간입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
+    })
     @PostMapping("/{officeId}/booking")
-    public ResponseCustom bookOffice(@PathVariable(name = "officeId") Long officeId,
-                                     @RequestBody @Valid OfficeReq officeReq){
+    public ResponseCustom bookOffice(
+            @Parameter(description = "(Long) 회의실 Id", example = "1") @PathVariable(name = "officeId") Long officeId,
+            @RequestBody @Valid OfficeReq officeReq){
         // 현재보다 과거 날짜로 등록하는 경우
         if(LocalDate.now().isAfter(officeReq.getDate())) throw new BaseException(BaseResponseCode.DATE_MUST_BE_THE_FUTURE);
         // 끝나는 시간이 시작시간보다 빠른경우
--- src/main/java/com/example/pladialmserver/office/dto/request/OfficeReq.java
+++ src/main/java/com/example/pladialmserver/office/dto/request/OfficeReq.java
@@ -1,5 +1,6 @@
 package com.example.pladialmserver.office.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -19,15 +20,19 @@ import static com.example.pladialmserver.global.Constants.TIME_PATTERN;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OfficeReq {
+    @Schema(type = "LocalDate(String)", description = "예약일자", example = "2023-09-02", required = true, pattern = DATE_PATTERN)
     @NotNull(message = "B0001")
     @DateTimeFormat(pattern = DATE_PATTERN)
     private LocalDate date;
+    @Schema(type = "LocalTime(String)", description = "예약시작시간", example = "11:00", required = true, pattern = TIME_PATTERN)
     @NotNull(message = "B0001")
     @DateTimeFormat(pattern = TIME_PATTERN)
     private LocalTime startTime;
+    @Schema(type = "LocalTime(String)", description = "예약종료시간", example = "12:00", required = true, pattern = TIME_PATTERN)
     @NotNull(message = "B0001")
     @DateTimeFormat(pattern = TIME_PATTERN)
     private LocalTime endTime;
+    @Schema(type = "String", description = "이용목적", maxLength = 30)
     @Size(max = 30, message = "B0002")
     private String memo;
 }
