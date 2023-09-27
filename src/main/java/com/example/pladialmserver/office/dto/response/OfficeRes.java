6d16f828fa41aa17fa2d48f89090c1f34ebee89c
iseunghag <qw0802@naver.com>
2023-09-27 15:40:22 +0900

    [PDS-11] fix:response에 id추가

--- src/main/java/com/example/pladialmserver/office/dto/response/OfficeRes.java
+++ src/main/java/com/example/pladialmserver/office/dto/response/OfficeRes.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 @Data
 @Builder
 public class OfficeRes {
+    private Long officeId;
     private String name;
     private String location;
     private Integer capacity;
@@ -20,6 +21,7 @@ public class OfficeRes {
 
     public static OfficeRes toDto(Office office, List<Facility> facilities,List<String> imgUrls){
         return OfficeRes.builder()
+                .officeId(office.getOfficeId())
                 .name(office.getName())
                 .location(office.getLocation())
                 .capacity(office.getCapacity())
