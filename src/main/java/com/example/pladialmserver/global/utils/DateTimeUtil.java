b803ba1af16a4b7472236f74e06d9b65cc4893e9
sojungpp <sophie2458@naver.com>
2023-10-01 02:10:01 +0900

    [PDS-100] feat: DateTimeUtil 생성

---
+++ src/main/java/com/example/pladialmserver/global/utils/DateTimeUtil.java
@@ -0,0 +1,28 @@
+package com.example.pladialmserver.global.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.example.pladialmserver.global.Constants.*;
+
+public class DateTimeUtil {
+
+    // localDate + localTime => string(YYYY-MM-DD HH:MM)
+    public static String dateAndTimeToString(LocalDate date, LocalTime time) {
+        return LocalDateTime.of(date, time).format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+    }
+
+    // localDate => string(YYYY-MM-DD)
+    public static String dateToString(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern(DATE_PATTERN));
+    }
+
+    // localTime => string(HH:MM)
+    public static String timeToString(LocalTime time) {
+        return time.format(DateTimeFormatter.ofPattern(TIME_PATTERN));
+    }
+
+
+}
