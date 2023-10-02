90c9467653eb9739b7b964c9b5d13082d0fbd173
chaerlo127 <monynony0203@gmail.com>
2023-10-03 00:50:01 +0900

    [PDS-51] fix: Resource 도메인 생성 충돌 해결

--- src/main/java/com/example/pladialmserver/booking/repository/ResourceBookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pladialmserver.booking.repository;
-
-import com.example.pladialmserver.booking.entity.ResourceBooking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceBookingRepository extends JpaRepository<ResourceBooking,Long> {
-}
