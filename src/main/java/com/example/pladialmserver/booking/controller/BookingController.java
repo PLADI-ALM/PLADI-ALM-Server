81629c7d5f514242b1c3d2bd1fbce54ac03fcca4
sojungpp <sophie2458@naver.com>
2023-10-03 03:27:27 +0900

    [PDS-74] feat: 자원 예약 개별 조회 API 스웨거 설명 추가

--- src/main/java/com/example/pladialmserver/booking/controller/BookingController.java
+++ src/main/java/com/example/pladialmserver/booking/controller/BookingController.java
@@ -75,6 +75,12 @@ public class BookingController {
     /**
      * 자원 예약 개별 조회
      */
+    @Operation(summary = "자원 예약 개별 조회", description = "자원 예약 내역을 개별 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "(S0001)요청에 성공했습니다."),
+            @ApiResponse(responseCode = "403", description = "(G0002)접근권한이 없습니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "404", description = "(B0006)존재하지 않는 예약입니다. (U0001)사용자를 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
+    })
     @GetMapping("/resources/{resourceBookingId}")
     public ResponseCustom<ResourceBookingDetailRes> getResourceBookingDetail(@Parameter(description = "(Long) 자원 예약 Id", example = "1") @PathVariable(name="resourceBookingId") Long resourceBookingId){
         // TODO 유저 ID 받아오는 로직 추가
--- src/main/java/com/example/pladialmserver/booking/dto/response/ResourceBookingDetailRes.java
+++ src/main/java/com/example/pladialmserver/booking/dto/response/ResourceBookingDetailRes.java
@@ -2,16 +2,22 @@ package com.example.pladialmserver.booking.dto.response;
 
 import com.example.pladialmserver.booking.entity.ResourceBooking;
 import com.example.pladialmserver.global.utils.DateTimeUtil;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ResourceBookingDetailRes {
+    @Schema(type = "String", description = "상태", example = "'예약대기' / '예약중' / '사용중' / '사용완료' / '예약취소'")
     private String status;
+    @Schema(type = "String", description = "예약일자(시작일)", example = "2023-10-01")
     private String startDate;
+    @Schema(type = "String", description = "예약일자(종료일)", example = "2023-10-02")
     private String endDate;
+    @Schema(type = "String", description = "반납일", example = "2023-10-02 14:00 / null")
     private String returnDateTime;
+    @Schema(type = "String", description = "이용목적", example = "10월 2일 촬영에 사용할 예정입니다.")
     private String memo;
 
     public static ResourceBookingDetailRes toDto(ResourceBooking resourceBooking) {
