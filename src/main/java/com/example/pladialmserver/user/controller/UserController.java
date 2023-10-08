6982d87aed0f5fcd4dad2eb8211db2903ce5d485
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-10-08 21:10:14 +0900

    [PDS-105] feat: feign client 테스트 로직 작성(addUser)

--- src/main/java/com/example/pladialmserver/user/controller/UserController.java
+++ src/main/java/com/example/pladialmserver/user/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.pladialmserver.user.controller;
 
+import com.example.pladialmserver.global.feign.dto.UserReq;
+import com.example.pladialmserver.global.response.ResponseCustom;
 import com.example.pladialmserver.user.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,9 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/add")
+    public ResponseCustom<?> addUser(@RequestBody UserReq userReq) {
+        userService.addUser(userReq);
+        return ResponseCustom.OK();
+    }
 }
--- src/main/java/com/example/pladialmserver/user/service/UserService.java
+++ src/main/java/com/example/pladialmserver/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.pladialmserver.user.service;
 
+import com.example.pladialmserver.global.feign.dto.UserReq;
+import com.example.pladialmserver.global.feign.feignClient.ArchivingServerClient;
 import com.example.pladialmserver.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ArchivingServerClient archivingServerClient;
 
+    public void addUser(UserReq userReq) {
+        archivingServerClient.addUser(userReq);
+    }
 }
