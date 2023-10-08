05afae2f2f4b408f12f5370830fc8e86f56765ef
iseunghag <qw0802@naver.com>
2023-10-08 21:09:39 +0900

    [PDS-102] fix:필요없는 임포트 제거

--- src/main/java/com/example/pladialmserver/booking/repository/resourceBooking/ResourceBookingRepository.java
+++ src/main/java/com/example/pladialmserver/booking/repository/resourceBooking/ResourceBookingRepository.java
@@ -1,9 +1,6 @@
 package com.example.pladialmserver.booking.repository.resourceBooking;
 
 import com.example.pladialmserver.booking.entity.ResourceBooking;
-import com.example.pladialmserver.global.entity.BookingStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
