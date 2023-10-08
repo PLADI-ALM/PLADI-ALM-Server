efaf1204a7a274875fa5c9371f7b0b5a7f4e9ef5
iseunghag <qw0802@naver.com>
2023-10-08 21:00:37 +0900

    [PDS-102] fix:url수정, 서비스 로직 bookservice로 변경, 오름차순 정렬 추가

--- src/main/java/com/example/pladialmserver/admin/BookingAdminController.java
+++ src/main/java/com/example/pladialmserver/admin/BookingAdminController.java
@@ -1,29 +1,24 @@
 package com.example.pladialmserver.admin;
 
-import com.example.pladialmserver.booking.dto.response.BookingRes;
+import com.example.pladialmserver.booking.service.BookingService;
 import com.example.pladialmserver.global.response.ResponseCustom;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "관리자 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/Admin")
-public class AdminController {
-    private final AdminService adminService;
+@RequestMapping("/admin/bookings")
+public class BookingAdminController {
+    private final BookingService bookingService;
+
 
     /**
      * 관리자 회의실 예약 목록 조회
@@ -32,10 +27,10 @@ public class AdminController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "(S0001)요청에 성공했습니다."),
     })
-    @GetMapping
+    @GetMapping("/offices")
     public ResponseCustom<Page<AdminBookingRes>> getBookingOffices(
             @PageableDefault(size = 8) Pageable pageable){
-        return ResponseCustom.OK(adminService.getBookingOffices(pageable));
+        return ResponseCustom.OK(bookingService.getBookingOffices(pageable));
     }
 
 
@@ -47,6 +42,7 @@ public class AdminController {
     /**
      * 관리자 회의실 상세 내역을 조회한다.
      */
+
 
 
     /**
