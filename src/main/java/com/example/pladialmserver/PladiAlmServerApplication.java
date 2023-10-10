f01c0bbfaae0603d2cc95817becc77ea0126e5c8
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-10-10 20:38:19 +0900

    [PDS-113] feat: 불필요한 import 문 제거

--- src/main/java/com/example/pladialmserver/PladiAlmServerApplication.java
+++ src/main/java/com/example/pladialmserver/PladiAlmServerApplication.java
@@ -1,13 +1,11 @@
 package com.example.pladialmserver;
 
 import com.example.pladialmserver.global.feign.feignClient.ArchivingServerClient;
-import com.example.pladialmserver.global.response.ResponseCustom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableScheduling
 @EnableAsync
