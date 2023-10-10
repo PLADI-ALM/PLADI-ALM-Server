cd7d076b8305db87ebb8eac1c26dd31072851114
sojungpp <sophie2458@naver.com>
2023-10-10 20:49:11 +0900

    [PDS-114] refactor: 주석 형식 통일

--- src/main/java/com/example/pladialmserver/office/service/OfficeService.java
+++ src/main/java/com/example/pladialmserver/office/service/OfficeService.java
@@ -14,7 +14,6 @@ import com.example.pladialmserver.office.entity.Office;
 import com.example.pladialmserver.office.entity.OfficeFacility;
 import com.example.pladialmserver.office.repository.OfficeRepository;
 import com.example.pladialmserver.user.entity.User;
-import com.example.pladialmserver.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,11 +29,13 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OfficeService {
-    private final UserRepository userRepository;
     private final OfficeRepository officeRepository;
     private final OfficeBookingRepository officeBookingRepository;
 
-    //전체 회의실 목록 조회 and 예약 가능한 회의실 목록 조회
+
+    /**
+     * 전체 회의실 목록 조회 and 예약 가능한 회의실 목록 조회
+     */
     public Page<OfficeRes> findAvailableOffices(LocalDate date, LocalTime startTime, LocalTime endTime, Pageable pageable) {
 
         Page<Office> allOffices;
@@ -61,7 +62,9 @@ public class OfficeService {
         });
     }
 
-     //회의실 개별 조회
+    /**
+     * 회의실 개별 조회
+     */
     public OfficeRes getOffice(Long officeId) {
         Office office = officeRepository.findByOfficeId(officeId)
                 .orElseThrow(() -> new BaseException(BaseResponseCode.OFFICE_NOT_FOUND));
