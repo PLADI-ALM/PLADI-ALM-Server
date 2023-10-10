ac94f7c9191b9e21e8de510b8028024ed57673df
sojungpp <sophie2458@naver.com>
2023-10-10 18:22:16 +0900

    [PDS-111] fix: 불필요한 예외처리 삭제

--- src/main/java/com/example/pladialmserver/booking/service/BookingService.java
+++ src/main/java/com/example/pladialmserver/booking/service/BookingService.java
@@ -212,19 +212,16 @@ public class BookingService {
     }
 
     public ResourceBookingDetailRes getResourceBookingDetailByAdmin(Long userId, Long resourceBookingId) {
-        User user = checkAdmin(userId);
+        checkAdmin(userId);
         ResourceBooking resourceBooking = resourceBookingRepository.findById(resourceBookingId)
                 .orElseThrow(() -> new BaseException(BaseResponseCode.BOOKING_NOT_FOUND));
-        if(!resourceBooking.getUser().equals(user)) throw new BaseException(BaseResponseCode.NO_AUTHENTICATION);
-
         return ResourceBookingDetailRes.toDto(resourceBooking);
     }
 
-    private User checkAdmin(Long userId) {
+    private void checkAdmin(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BaseException(BaseResponseCode.USER_NOT_FOUND));
         // 관리자 유무
         if(!user.getRole().equals(Role.ADMIN)) throw new BaseException(BaseResponseCode.NO_AUTHENTICATION);
-        return user;
     }
 }
