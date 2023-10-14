6d577b541919c2a943e8a60c57a76811e3a6f3c2
iseunghag <qw0802@naver.com>
2023-10-14 23:19:33 +0900

    [PDS-11] feat:회의실 목록 조회 시설 검색 기능 추가

--- src/main/java/com/example/pladialmserver/office/repository/OfficeRepository.java
+++ src/main/java/com/example/pladialmserver/office/repository/OfficeRepository.java
@@ -4,6 +4,8 @@ import com.example.pladialmserver.office.entity.Office;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +14,17 @@ public interface OfficeRepository extends JpaRepository<Office, Long> {
     Optional<Office> findByOfficeId(Long officeId);
     Page<Office> findAllByOfficeIdNotIn(List<Long> ids, Pageable pageable);
     Page<Office> findAll(Pageable pageable);
+    @Query("SELECT ofc.office " +
+            "FROM OfficeFacility ofc " +
+            "WHERE ofc.facility.name = :facilityName")
+    Page<Office> findByFacilityName(@Param("facilityName") String facilityName, Pageable pageable);
+
+    @Query("SELECT ofc.office " +
+            "FROM OfficeFacility ofc " +
+            "WHERE ofc.facility.name = :facilityName" +
+            " AND ofc.office.officeId " +
+            "NOT IN :bookedOfficeIds")
+    Page<Office> findByFacilityNameAndOfficeIdNotIn(@Param("facilityName") String facilityName, @Param("bookedOfficeIds") List<Long> bookedOfficeIds, Pageable pageable);
+
 
 }
