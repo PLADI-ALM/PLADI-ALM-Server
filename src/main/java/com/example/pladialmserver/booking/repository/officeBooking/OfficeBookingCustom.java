e1ca7192892de7ba40dcbd8102b0c85f159bac18
chaerlo127 <monynony0203@gmail.com>
2023-10-17 01:30:52 +0900

    [PDS-15] hotfix: 쿼리 officeId 포함하여 정보 전달

--- src/main/java/com/example/pladialmserver/booking/repository/officeBooking/OfficeBookingCustom.java
+++ src/main/java/com/example/pladialmserver/booking/repository/officeBooking/OfficeBookingCustom.java
@@ -3,6 +3,7 @@ package com.example.pladialmserver.booking.repository.officeBooking;
 import com.example.pladialmserver.booking.dto.response.BookingRes;
 import com.example.pladialmserver.booking.entity.OfficeBooking;
 import com.example.pladialmserver.global.entity.BookingStatus;
+import com.example.pladialmserver.office.entity.Office;
 import com.example.pladialmserver.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +14,7 @@ import java.util.List;
 
 public interface OfficeBookingCustom {
     Page<BookingRes> getBookingsByUser(User user, Pageable pageable);
-    Boolean existsByDateAndTime(LocalDate date, LocalTime startTime, LocalTime endTime);
+    Boolean existsByDateAndTime(Office office, LocalDate date, LocalTime startTime, LocalTime endTime);
     List<OfficeBooking> findByStatusAndDateAndEndTime(BookingStatus status);
     List<OfficeBooking> findByStatusAndDateAndStartTime(BookingStatus status);
 
