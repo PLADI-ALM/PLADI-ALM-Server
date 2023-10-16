ba3645afbf5220eee4b473b6812ae3e4604e2db7
chaerlo127 <monynony0203@gmail.com>
2023-10-16 13:45:00 +0900

    [PDS-125] feat: 토큰 재발급 API + 스웨거 설정

--- src/main/java/com/example/pladialmserver/user/dto/TokenDto.java
+++ src/main/java/com/example/pladialmserver/user/dto/TokenDto.java
@@ -5,11 +5,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class TokenDto {
     @Schema(type = "String", description = "AccessToken", example = "bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTY3NTg2OTcsInVzZXJJZHgiOjEsInN1YiI6IjEifQ.DSBuBlStkjhT05vuzjWd-cg7naG5KikUxII734u3nUw")
+    @NotBlank(message = "G0001")
     private String accessToken;
+    @NotBlank(message = "G0001")
     @Schema(type = "String", description = "RefreshToken", example = "bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTY3NTg2OTcsInVzZXJJZHgiOjEsInN1YiI6IjEifQ.DSBuBlStkjhT05vuzjWd-cg7naG5KikUxII734u3nUw")
     private String refreshToken;
     @Schema(type = "String", description = "사용자 역할", example = "BASIC / ADMIN")
