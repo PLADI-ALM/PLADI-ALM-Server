e0e1f822ff9a081c2a91e9e3629c6e1ea761ca8b
sojungpp <sophie2458@naver.com>
2023-10-21 15:16:14 +0900

    [PDS-132] fix: 관리자 회의실 예약 상세 조회 API 수정

--- src/main/java/com/example/pladialmserver/booking/dto/response/OfficeBookingDetailRes.java
+++ src/main/java/com/example/pladialmserver/booking/dto/response/OfficeBookingDetailRes.java
@@ -14,6 +14,10 @@ public class OfficeBookingDetailRes {
 
     @Schema(type = "Long", description = "회의실 id", example = "1")
     private Long officeId;
+    @Schema(type = "String", description = "예약자 이름", example = "박소정")
+    private String reservatorName;
+    @Schema(type = "String", description = "예약자 연락처", example = "010-1111-1004")
+    private String reservatorPhone;
     @Schema(type = "LocalDate(String)", description = "예약일자", example = "2023-09-02", required = true, pattern = DATE_PATTERN)
     private String date;
     @Schema(type = "LocalTime(String)", description = "예약시작시간", example = "11:00")
@@ -28,6 +32,8 @@ public class OfficeBookingDetailRes {
     public static OfficeBookingDetailRes toDto(OfficeBooking officeBooking) {
         return OfficeBookingDetailRes.builder()
                 .officeId(officeBooking.getOffice().getOfficeId())
+                .reservatorName(officeBooking.getUser().getName())
+                .reservatorPhone(officeBooking.getUser().getPhone())
                 .date(DateTimeUtil.dateToString(officeBooking.getDate()))
                 .startTime(DateTimeUtil.timeToString(officeBooking.getStartTime()))
                 .endTime(DateTimeUtil.timeToString(officeBooking.getEndTime()))
