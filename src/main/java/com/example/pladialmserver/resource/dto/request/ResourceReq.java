3bac43e94dbea826fdc433ac163b21bdecdec1ca
sojungpp <sophie2458@naver.com>
2023-10-29 00:59:40 +0900

    [PDS-72] fix: LocalDateTime 바인딩 수정

--- src/main/java/com/example/pladialmserver/resource/dto/request/ResourceReq.java
+++ src/main/java/com/example/pladialmserver/resource/dto/request/ResourceReq.java
@@ -1,27 +1,27 @@
 package com.example.pladialmserver.resource.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-import static com.example.pladialmserver.global.Constants.*;
+import static com.example.pladialmserver.global.Constants.DATE_HOUR_PATTERN;
 
 @Getter
 @NoArgsConstructor
 public class ResourceReq {
-    @Schema(type = "LocalDateTime(String)", description = "예약 시작일", example = "2023-10-10 13:00", required = true, pattern = LOCAL_DATE_TIME_PATTERN)
+    @Schema(type = "LocalDateTime(String)", description = "예약 시작일", example = "2023-10-10 13", required = true, pattern = DATE_HOUR_PATTERN)
     @NotNull(message = "B0010")
-    @DateTimeFormat(pattern = LOCAL_DATE_TIME_PATTERN)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_HOUR_PATTERN)
     private LocalDateTime startDateTime;
 
-    @Schema(type = "LocalDateTime(String)", description = "예약 종료일", example = "2023-10-12 15:00", required = true, pattern = LOCAL_DATE_TIME_PATTERN)
+    @Schema(type = "LocalDateTime(String)", description = "예약 종료일", example = "2023-10-12 15", required = true, pattern = DATE_HOUR_PATTERN)
     @NotNull(message = "B0010")
-    @DateTimeFormat(pattern = LOCAL_DATE_TIME_PATTERN)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_HOUR_PATTERN)
     private LocalDateTime endDateTime;
 
     @Schema(type = "String", description = "이용목적", maxLength = 30)
