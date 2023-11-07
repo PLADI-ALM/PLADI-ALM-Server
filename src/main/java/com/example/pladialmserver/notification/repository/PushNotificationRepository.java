c20a60681dd7640aa7ad329978e531824b41b7de
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-11-07 21:28:57 +0900

    [PDS-163] feat: 유저 알림 목록 조회 API 기능 구현

--- src/main/java/com/example/pladialmserver/notification/repository/PushNotificationRepository.java
+++ src/main/java/com/example/pladialmserver/notification/repository/PushNotificationRepository.java
@@ -1,9 +1,13 @@
 package com.example.pladialmserver.notification.repository;
 
 import com.example.pladialmserver.notification.entity.PushNotification;
+import com.example.pladialmserver.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PushNotificationRepository extends JpaRepository<PushNotification, Long> {
+    Page<PushNotification> findByUserAndIsEnableOrderByCreatedAtDesc(User user, Boolean isEnable, Pageable pageable);
 }
