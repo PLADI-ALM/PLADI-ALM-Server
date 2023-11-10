4ca05a41113acefd92d5e1239bf4fb35632023b5
iseunghag <qw0802@naver.com>
2023-11-10 16:10:08 +0900

    [PDS-171] feat:테스트 코드 초기설정 구축

---
+++ src/test/java/com/example/pladialmserver/resource/controller/ResourceControllerTest.java
@@ -0,0 +1,6 @@
+package com.example.pladialmserver.resource.controller;
+
+import com.example.pladialmserver.global.ControllerTestSupport;
+
+class ResourceControllerTest extends ControllerTestSupport {
+}
