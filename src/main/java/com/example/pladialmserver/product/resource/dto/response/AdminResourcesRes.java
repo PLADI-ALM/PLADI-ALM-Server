5491802a511b493a43b87c24342c570a1a74c90d
psyeon1120 <psycsh@naver.com>
2023-11-16 16:27:45 +0900

    [PDS-188] hotfix: 장비/차량 목록 조회 id로 통일

--- src/main/java/com/example/pladialmserver/product/resource/dto/response/AdminResourcesRes.java
+++ src/main/java/com/example/pladialmserver/product/resource/dto/response/AdminResourcesRes.java
@@ -8,22 +8,22 @@ import lombok.Getter;
 @Getter
 @Builder
 public class AdminResourcesRes {
-    @Schema(type = "Long", description = "장비 Id", example = "1")
-    private Long resourceId;
+    @Schema(type = "Long", description = "장비/차량 Id", example = "1")
+    private Long id;
 
-    @Schema(type = "String", description = "장비명", example = "MacBook Pro")
+    @Schema(type = "String", description = "장비/차량명", example = "MacBook Pro")
     private String name;
 
     @Schema(type = "String", description = "제조사", example = "Apple")
     private String manufacturer;
 
-    @Schema(type = "String", description = "보관장소", example = "3층 A홀")
+    @Schema(type = "String", description = "현재위치", example = "3층 A홀")
     private String location;
 
     @Schema(type = "String", description = "책임자 이름", example = "박소정")
     private String responsibilityName;
 
-    @Schema(type = "String", description = "책임자 연락처", example = "010-1004-10004")
+    @Schema(type = "String", description = "책임자 연락처", example = "010-1004-1004")
     private String responsibilityPhone;
 
     @Schema(type = "String", description = "설명", example = "맥북프로사줘")
@@ -33,8 +33,8 @@ public class AdminResourcesRes {
     private Boolean isActive;
 
     @QueryProjection
-    public AdminResourcesRes(Long resourceId, String name, String manufacturer, String location, String responsibilityName, String responsibilityPhone, String description, Boolean isActive) {
-        this.resourceId = resourceId;
+    public AdminResourcesRes(Long id, String name, String manufacturer, String location, String responsibilityName, String responsibilityPhone, String description, Boolean isActive) {
+        this.id = id;
         this.name = name;
         this.manufacturer = manufacturer;
         this.location = location;
