3f1cfe448794ef237e87dd40f547a90ff674d9dd
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-11-28 18:40:49 +0900

    [PDS-205] feat: 카테고리 목록 조회 API 리스폰스 스웨거 명시

--- src/main/java/com/example/pladialmserver/equipment/dto/response/CategoryNameRes.java
+++ src/main/java/com/example/pladialmserver/equipment/dto/response/CategoryNameRes.java
@@ -1,11 +1,13 @@
 package com.example.pladialmserver.equipment.dto.response;
 
 import com.example.pladialmserver.equipment.entity.EquipmentCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class CategoryNameRes {
+    @Schema(type = "String", description = "비품 카테고리 이름", example = "기타")
     private String name;
 
     @Builder
--- src/main/java/com/example/pladialmserver/equipment/dto/response/EquipmentCategoryRes.java
+++ src/main/java/com/example/pladialmserver/equipment/dto/response/EquipmentCategoryRes.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 @Data
 public class EquipmentCategoryRes {
-    @Schema(type = "String", description = "비품 카테고리 이름", example = "기타")
+    @Schema(type = "List<CategoryNameRes>", description = "카테고리 목록 리스트", example = "기타")
     private List<CategoryNameRes> categoryNames;
 
     @Builder
